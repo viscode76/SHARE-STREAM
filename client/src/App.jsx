971ccae03f320cf8227f8e6d391d6d47d9c215a3
viscode76 <justfortest9001@gmail.com>
2026-07-05 19:48:54 +0530

    fixed the router issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import NetflixPage from './pages/Netflix'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Test from './pages/Test'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
 
   return (
     <>
@@ -16,7 +17,15 @@ function App() {
         <Route path='/stream' element={<NetflixPage />} />
         <Route path='/test' element={<Test />} />
       </Routes>
-      Coming Soon Nice
+      <button onClick={() => navigate("/test")}
+        className='px-6 py-3 rounded-2xl shadow-lg bg-black text-white'>
+        Test
+      </button>
+
+      <button onClick={() => navigate("/stream")}
+        className='px-6 py-3 rounded-2xl shadow-lg bg-black text-white'>
+        Stream
+      </button>
     </>
   )
 }
